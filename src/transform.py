50315fd0389c1afa8b0b68448ad97823c8a3cd4f
beatrizhernandezvaquerizasvl-beep <beatrizhernandezvaquerizasvl@gmail.com>
2025-09-24 14:53:57 +0200

    Create transform.py

---
+++ src/transform.py
@@ -0,0 +1,8 @@
+# transform.py
+# Normalización y preparación para capas bronze/silver.
+
+def normalize_data(df):
+    """
+    Estandariza tipos, nombres y prepara datos limpios.
+    """
+    pass
